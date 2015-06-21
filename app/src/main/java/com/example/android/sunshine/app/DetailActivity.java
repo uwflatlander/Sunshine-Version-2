4e9c353f39e217eafc55fb019371011feeed7056
uwflatlander <tschaab@gmail.com>
2015-06-21 00:50:31 -0500

    3-7 Launch Settings activity

--- app/src/main/java/com/example/android/sunshine/app/DetailActivity.java
+++ app/src/main/java/com/example/android/sunshine/app/DetailActivity.java
@@ -16,6 +16,7 @@
 
 package com.example.android.sunshine.app;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
@@ -53,6 +54,8 @@ public class DetailActivity extends ActionBarActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            Intent settings = new Intent(this,SettingsActivity.class);
+            startActivity(settings);
             return true;
         }
 
