1aafd6480bb689b721c2290862c058143d26ccbb
uwflatlander <tschaab@gmail.com>
2015-06-21 03:33:38 -0500

    3-13 Share Option revisions

--- app/src/main/java/com/example/android/sunshine/app/DetailFragment.java
+++ app/src/main/java/com/example/android/sunshine/app/DetailFragment.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.MenuItemCompat;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -17,6 +18,7 @@ import android.widget.TextView;
  * A placeholder fragment containing a simple view.
  */
 public class DetailFragment extends Fragment {
+    private static final String LOG_TAG = DetailFragment.class.getSimpleName();
 
     private ShareActionProvider mShareActionProvider;
     private static String FORECAST_SHARE_HASHTAG = " #SunshineApp";
@@ -33,17 +35,27 @@ public class DetailFragment extends Fragment {
 
     @Override
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
-            inflater.inflate(R.menu.detailfragment, menu);
+        inflater.inflate(R.menu.detailfragment, menu);
 
         MenuItem item = menu.findItem(R.id.action_share_forecast);
 
         mShareActionProvider = (ShareActionProvider) MenuItemCompat.getActionProvider(item);
+
+        if (mShareActionProvider != null) {
+            mShareActionProvider.setShareIntent(getShareIntent());
+        } else
+        {
+            Log.d(LOG_TAG, "Share Action Provider Missing!?");
+        }
+        super.onCreateOptionsMenu(menu, inflater);
+    }
+
+    private Intent getShareIntent(){
         Intent shareIntent = new Intent(Intent.ACTION_SEND);
         shareIntent.setType("text/plain");
+        shareIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET);
         shareIntent.putExtra(Intent.EXTRA_TEXT, mForecast + FORECAST_SHARE_HASHTAG);
-        mShareActionProvider.setShareIntent(shareIntent);
-
-        super.onCreateOptionsMenu(menu, inflater);
+        return shareIntent;
     }
 
 
